8541fdc4cbddf97b08dd4b70aec45d2b66bae78b
HEMAMALINIsaravanan <hemamalini.sece@gmail.com>
2017-07-22 10:12:06 +0530

    Create pattern1.c

---
+++ pattern1.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int a=0,i;
+    while(a<5)
+    {
+    for(i=0;i<5;i++)
+    {
+        printf("*");
+    }
+    printf("\n");
+    a++;
+    }
+    return 0;
+}
